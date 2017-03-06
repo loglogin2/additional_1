3b5b9c0aaf813c19c1e09255023e93348f596562
loglogin2 <i9991559@gmail.com>
2017-03-06 18:59:46 +0300

    Closure/feature: implement all

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function sum() {
-  // your solution
+module.exports = function sum(...a)
+{
+    // your solution
+    if( a.length == 0 ) {
+        return 0;
+    }
+    var sum = 0 ;
+    for( var i = 0 ; i < a.length ; i++ ){
+        sum += a[i];
+    }
+    function summ(...b){
+        for ( var i = 0 ; i < b.length ; i ++ ){
+            sum += b[i];
+        }
+        return summ;
+    }
+    summ.toString = function(){
+        return sum;
+    }
+    return summ;
 }
